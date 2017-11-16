0120c0e2cb3b638fb0b6bbe4b2b3ff60b26ef800
sohel2178 <sohel.ahmed2178@gmail.com>
2017-11-16 17:06:49 +0600

    16/11/2017

--- app/src/main/java/com/example/nlpc06/tabview/Fragments/DoctorFragment.java
+++ app/src/main/java/com/example/nlpc06/tabview/Fragments/DoctorFragment.java
@@ -1,30 +1,119 @@
 package com.example.nlpc06.tabview.Fragments;
 
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.provider.Settings;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.example.nlpc06.tabview.R;
+
+import java.io.File;
+import java.io.FileOutputStream;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class DoctorFragment extends Fragment {
+public class DoctorFragment extends Fragment implements OnClickListener {
+    private static final int CAMERA_REQ=1100;
+
+    private Button btnOpenCamera;
+    private ImageView ivImage;
+
+
 
 
     public DoctorFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        //Log.d("GGG",root);
+
+
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_doctor, container, false);
+        View view = inflater.inflate(R.layout.fragment_doctor, container, false);
+
+        initView(view);
+
+        return view;
     }
 
+    private void initView(View view) {
+        btnOpenCamera = view.findViewById(R.id.open_camera);
+        ivImage = view.findViewById(R.id.image);
+        btnOpenCamera.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent,CAMERA_REQ);
+
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if(requestCode==CAMERA_REQ){
+            Log.d("HHH","Found");
+            Bitmap bitmap = data.getParcelableExtra("data");
+
+            saveBitmap(bitmap);
+
+            ivImage.setImageBitmap(bitmap);
+        }
+    }
+
+    private void saveBitmap(Bitmap bitmap){
+        String root = Environment.getExternalStorageDirectory().toString();
+
+        String appName =getString(R.string.app_name);
+
+        File myDir = new File(root + "/"+appName);
+
+        if(!myDir.exists()){
+            myDir.mkdirs();
+        }
+
+        long time = System.currentTimeMillis();
+
+        File file = new File(myDir,time+".jpg");
+
+        if(file.exists()){
+            file.delete();
+        }
+
+        try {
+            FileOutputStream out = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+            out.flush();
+            out.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
--- app/src/main/java/com/example/nlpc06/tabview/MainActivity.java
+++ app/src/main/java/com/example/nlpc06/tabview/MainActivity.java
@@ -1,16 +1,25 @@
 package com.example.nlpc06.tabview;
 
+import android.Manifest;
+import android.os.Environment;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.nlpc06.tabview.Adater.HospitalAdapter;
 import com.example.nlpc06.tabview.Fragments.CabinFragment;
 import com.example.nlpc06.tabview.Fragments.DoctorFragment;
 import com.example.nlpc06.tabview.Fragments.PatientFragment;
 
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.EasyPermissions;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final int ALL_PERMISSION =3000;
 
     private TabLayout tabLayout;
     private ViewPager viewPager;
@@ -20,12 +29,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getAllPermission();
+
+
+    }
+
+    public void init(){
         tabLayout = (TabLayout) findViewById(R.id.tabs);
         viewPager = (ViewPager) findViewById(R.id.view_pager);
-
         setupViewPager();
-
         tabLayout.setupWithViewPager(viewPager);
+
+
     }
 
     private void setupViewPager() {
@@ -34,8 +49,35 @@ public class MainActivity extends AppCompatActivity {
         adapter.addFragment(new DoctorFragment(),"Doctor");
         adapter.addFragment(new PatientFragment(),"Patient");
         adapter.addFragment(new CabinFragment(),"Cabin");
-        
+
         viewPager.setAdapter(adapter);
 
     }
+
+    @AfterPermissionGranted(ALL_PERMISSION)
+    private void getAllPermission(){
+        String[] perms = {Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE,Manifest.permission.WRITE_EXTERNAL_STORAGE};
+
+        if (EasyPermissions.hasPermissions(this, perms)) {
+            // Already have permission, do the thing
+            // ...
+
+            init();
+        } else {
+            // Do not have permissions, request them now
+            EasyPermissions.requestPermissions(this, "All Permission",
+                    ALL_PERMISSION, perms);
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        // Forward results to EasyPermissions
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
+    }
+
+
 }
